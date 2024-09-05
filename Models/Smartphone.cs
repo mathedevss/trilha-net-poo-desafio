dc1b642b5a59f4c29cc3263d03fb211efa1ff703
Matheus Medeiros <matheussouza1760@gmail.com>
2024-09-05 14:36:38 -0300

    Desafio Concluido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,18 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo { get; set; }
+        protected string Mei { get; set; }
+        protected int Memoria { get; set; }
 
-        public Smartphone(string numero)
+
+        public Smartphone(string numero, string modelo, string mei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Mei = mei;
+            Modelo = modelo;
+            Memoria = memoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia tijolex");
+Nokia tijolex = new Nokia("213 1542-2573", "Tijolão", "1111111", 64);
+tijolex.Ligar();
+tijolex.InstalarAplicativo("Brawl stars");
+
+Console.WriteLine("");
+
+Console.WriteLine("Iphone Quadrado");
+Iphone quadrado = new Iphone("213 1542-2573", "Quadrado", "1111111", 256);
+quadrado.ReceberLigacao();
+quadrado.InstalarAplicativo("Instagram");
